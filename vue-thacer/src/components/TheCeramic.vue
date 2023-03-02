69cc703f03098ebbd275fb374a99f42efa4a42a3
francois <francoiscrouzet@posteo.de>
2023-03-02 17:42:50 +0000

    Modify 4 files with default vue prettierc config

--- vue-thacer/src/components/TheCeramic.vue
+++ vue-thacer/src/components/TheCeramic.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: 'TheCeramic',
+  name: 'TheCeramic'
 }
 </script>
 
--- vue-thacer/src/components/TheMap.vue
+++ vue-thacer/src/components/TheMap.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: 'TheMap',
+  name: 'TheMap'
 }
 </script>
 
