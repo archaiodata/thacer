74e9efee58311ce89900d6b6a237c3267deab56a
francois <francoiscrouzet@posteo.de>
2023-03-05 12:08:54 +0000

    Copy and paste the html from old index.html

--- vue-thacer/src/components/TheMap.vue
+++ vue-thacer/src/components/TheMap.vue
@@ -1,5 +1,28 @@
 <template>
-  <header>The MAP</header>
+  <main role="main">
+    <div id="map-container">
+      <div id="map" class="map"></div>
+    </div>
+  </main>
+  <div id="encart-container">
+    <div
+      id="encart"
+      class="encart leaflet-control-layers leaflet-container"
+      style="
+        position: absolute;
+        bottom: 30px;
+        right: 0px;
+        width: 175px;
+        max-height: 50%;
+        overflow: auto;
+        margin-right: 10px;
+        padding: 6px 10px 6px 6px;
+      "
+    >
+      Non localisés
+      <div id="nonloc" class="d-flex flex-wrap"></div>
+    </div>
+  </div>
 </template>
 
 <script>
