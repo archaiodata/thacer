0f346fbc0baa8802e0037ee9ac10ebd73ded6595
francois <francoiscrouzet@posteo.de>
2023-03-07 17:17:51 +0000

    Refactor createOverlayMaps for clarity

--- vue-thacer/src/assets/js/thacer-map.js
+++ vue-thacer/src/assets/js/thacer-map.js
@@ -1,6 +1,6 @@
 import * as overlay from '@/assets/js/thacer-map-create-overlay'
 
-export function thacerMap() {
+export function thacerMap () {
   /* global L */
   L.mapbox.accessToken =
     'pk.eyJ1IjoianNncm9zIiwiYSI6ImNqOHQ0azNjcDBoYTEycXF1dzB0MzN4cDEifQ.DdPsBcV1XpWefQUPmBg9QA'
@@ -11,10 +11,11 @@ export function thacerMap() {
 
   let overlayMaps = createOverlayMaps(map)
 
-  L.control.layers(baseMaps, overlayMaps, { collapsed: false, sortLayers: true }).addTo(map)
+  L.control.layers(baseMaps, overlayMaps,
+    { collapsed: false, sortLayers: true }).addTo(map)
 }
 
-function createMap(defaultLayer) {
+function createMap (defaultLayer) {
   return {
     center: [40.78, 24.71],
     zoom: 15,
@@ -28,13 +29,14 @@ function createMap(defaultLayer) {
   }
 }
 
-function createBaseMaps() {
-  let googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    attribution:
-      'Imagery © <a href="https://www.google.com/help/legalnotices_maps.html">Google Maps</a>',
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-  })
+function createBaseMaps () {
+  let googleSat = L.tileLayer(
+    'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      attribution:
+        'Imagery © <a href="https://www.google.com/help/legalnotices_maps.html">Google Maps</a>',
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    })
   let outdoors = L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoianNncm9zIiwiYSI6ImNqOHQ0azNjcDBoYTEycXF1dzB0MzN4cDEifQ.DdPsBcV1XpWefQUPmBg9QA',
     {
@@ -55,28 +57,38 @@ function createBaseMaps() {
   }
 }
 
-function createOverlayMaps(map) {
+function createOverlayMaps (map) {
+  // Ceram - always displayed
   let markersCeram = overlay.createMarkersCeram()
   let ceram = overlay.createOverlayCeram(markersCeram, map)
   overlay.designMarkersCeram(ceram)
-  map.addLayer(markersCeram) // Always displayed
+  map.addLayer(markersCeram)
 
+  // Adelt and Tours - always displayed (except on some zoom level for ADelt)
+  overlay.createOverlayADelt(map).addTo(map)
+  overlay.createOverlayTours().addTo(map)
+
+  // Chronique - hid-able/show-able in the control
   let markersChronique = overlay.createMarkersChronique()
   overlay.createOverlayChronique(markersChronique)
 
-  overlay.createOverlayADelt(map).addTo(map) // Always displayed (except on some zoom level)
-  overlay.createOverlayTours().addTo(map) // Always displayed
+  // The rest - hid-able/show-able in the control
+  let vestiges = overlay.createOverlayVestiges()
+  let secteur = overlay.createOverlaySecteurs(ceram, markersCeram, map)
+  let khalil = overlay.createOverlayKhalil(map)
+  let orthophotoAgora = overlay.createOverlayOrthophotoAgora()
+  let ateliersAmphoriques = overlay.createOverlayAteliersAmphoriques()
+  let echantillonsGeol = overlay.createOverlayEchantillonsGeol()
 
+  // Finally, return the list of overlays which will be hid-able/show-able in the control
   return {
-    'Vestiges antiques': overlay.createOverlayVestiges().addTo(map), // Display enabled by default
-    'Secteurs de fouille et GTh': overlay
-      .createOverlaySecteurs(ceram, markersCeram, map)
-      .addTo(map), // Display enabled by default
-    'Plan Khalil 1954': overlay.createOverlayKhalil(map),
+    'Vestiges antiques': vestiges.addTo(map), // Display on  by default,
+    'Secteurs de fouille et GTh': secteur.addTo(map), // Display on by default,
+    'Plan Khalil 1954': khalil,
     'Chronique des fouilles': markersChronique,
     // 'Plan SIG agora': SIG_thasos,
-    'Orthophoto agora EfA': overlay.createOverlayOrthophotoAgora(),
-    'Ateliers amphoriques': overlay.createOverlayAteliersAmphoriques(),
-    'Echantillons géologiques': overlay.createOverlayEchantillonsGeol()
+    'Orthophoto agora EfA': orthophotoAgora,
+    'Ateliers amphoriques': ateliersAmphoriques,
+    'Echantillons géologiques': echantillonsGeol
   }
 }
