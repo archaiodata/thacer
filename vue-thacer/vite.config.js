25aeaaf9e53a1337b5f96e0808168fee93c10332
François Crouzet <francoiscrouzet@posteo.de>
2023-03-14 12:05:37 +0100

    #31 Add a proxy to fix the cors error (#32)
    **Issue** :  `https://github.com/archaiodata/thacer/issues/31`
    
    **Description** :    
    We add a specific development config which allow to access teh
    production API through a proxy, hence removing the cors error we had in
    dev env

--- vue-thacer/vite.config.js
+++ vue-thacer/vite.config.js
@@ -4,11 +4,30 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  let config = {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
+
+  if (command === 'serve') {
+    // In dev environment, we use a proxy to avoid cors errors :
+    config.server = {
+      proxy: {
+        '/proxys_replacement_key_for_api_path_in_dev': {
+          target: 'http://thacer.archaiodata.com/API/',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/proxys_replacement_key_for_api_path_in_dev/, '')
+        },
+        cors: false
+      }
+    }
+  }
+
+  return config
 })
