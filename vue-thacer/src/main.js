75aa9206c71c437c2fb323fc8c54c638df7e90b1
François Crouzet <francoiscrouzet@posteo.de>
2023-03-15 12:03:20 +0000

    #39 Add sentry to project (#66)
    **Issue** :  `https://github.com/archaiodata/thacer/issues/39`
    
    **Description** :    
    Added Sentry. Set the development environment. For the moment, sending
    manually error works, but automatically catching errors doesn't seem to
    work.

--- vue-thacer/src/main.js
+++ vue-thacer/src/main.js
@@ -1,10 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as Sentry from '@sentry/vue'
+import { BrowserTracing } from '@sentry/tracing'
 
 import './assets/main.css'
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  environment: import.meta.env.MODE,
+  dsn: 'https://720d793acf694bf79f43181d0663f880@o4504837917048832.ingest.sentry.io/4504837920587776',
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: ['localhost', '127.0.0.1', 'thacer.archaiodata.com', /^\//]
+    })
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
 
 app.use(router)
 
