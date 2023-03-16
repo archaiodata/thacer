45985c36e748c9b0e61b52a48e2065e1b754efa5
François Crouzet <francoiscrouzet@posteo.de>
2023-03-16 09:24:44 +0000

    #36 refactor the search code to allow future multi criterium search (#40)
    **Issue** :  `https://github.com/archaiodata/thacer/issues/36`
    
    **Description** :    
    This PR put the search mechanisms for the input search, which were used
    in 2 places (for localised and unlocalised elements), in a separate
    independent function.
    It will allow then to more easily implement multi criterium search.
    Please note that both search were slightly different, they are now the
    same (I kept the search which was used for localised element)

--- vue-thacer/src/assets/js/thacer-map-setup-search.js
+++ vue-thacer/src/assets/js/thacer-map-setup-search.js
@@ -1,3 +1,25 @@
+import { isObject } from '@/assets/js/utils.js'
+import { setCeramLayer } from '@/assets/js/thacer-map-create-layer'
+
+const doesCeramObjectPassesSearch = (ceramObject, searchString) => {
+  if (!isObject(ceramObject)) {
+    console.error(`Error in doesCeramObjectPassFilter, ceramObject is not an object :`, ceramObject)
+    return false
+  }
+
+  const isValueContainedInIdentification =
+    ceramObject['Identification'] &&
+    ceramObject['Identification'].toString().toLowerCase().includes(searchString)
+
+  const isValueEqualsPi = ceramObject['Pi'] && ceramObject['Pi'] === searchString
+
+  const isValueContainedInDescription =
+    ceramObject['Description'] &&
+    ceramObject['Description'].toString().toLowerCase().includes(searchString)
+
+  return isValueContainedInIdentification || isValueEqualsPi || isValueContainedInDescription
+}
+
 export function setupSearchCeramByText(markerClusterGroupCeram, map, featureLayerCeram) {
   let filterInput = document.getElementById('filter-input')
   filterInput.addEventListener('keyup', function (e) {
@@ -11,25 +33,19 @@ export function setupSearchCeramByText(markerClusterGroupCeram, map, featureLaye
           Object.keys(json.features).forEach((k) => {
             let obj = json.features[k]
 
-            if (
-              (obj.properties.x == 0 && obj.properties.Pi == value) ||
-              (obj.properties.x == 0 &&
-                obj.properties.Identification.toString().toLowerCase().indexOf(value) > -1)
-            ) {
-              if (obj.properties.Pi != null) {
-                document.getElementById('nonloc').innerHTML += [
-                  '<a class="unlocalised-tag px-1 m-0 border border-white" href="ceram?ID=' +
-                    obj.properties.ID +
-                    '&ANA=8888880&INV=88880">Π' +
-                    obj.properties.Pi +
-                    '</a>'
-                ]
-              } else {
+            if (obj.properties.x == 0) {
+              if (doesCeramObjectPassesSearch(obj.properties, value)) {
+                let label
+                if (obj.properties.Pi != null) {
+                  label = 'Π' + obj.properties.Pi
+                } else {
+                  label = obj.properties.Inv_Fouille
+                }
                 document.getElementById('nonloc').innerHTML += [
                   '<a class="unlocalised-tag px-1 m-0 border border-white" href="ceram?ID=' +
                     obj.properties.ID +
                     '&ANA=8888880&INV=88880">' +
-                    obj.properties.Inv_Fouille +
+                    label +
                     '</a>'
                 ]
               }
@@ -42,72 +58,10 @@ export function setupSearchCeramByText(markerClusterGroupCeram, map, featureLaye
       featureLayerCeram.loadURL(import.meta.env.VITE_API_URL + 'geojson/ceram.geojson') // je ne sais pourquoi j'avais mis ça là, déjà fait plus haut mais sinon map.addlayer ne marche pas
 
       featureLayerCeram
-        .setFilter(function (e) {
-          let return1
-          let return2
-          let return3
-          if (e.properties['Identification'] !== null) {
-            return1 = e.properties['Identification'].toString().toLowerCase().indexOf(value) > -1
-          }
-          if (e.properties['Pi'] !== null) {
-            return2 = e.properties['Pi'] == value
-          } else {
-            return2 = return1
-          }
-          if (e.properties['Description'] !== null) {
-            return3 = e.properties['Description'].toString().toLowerCase().indexOf(value) > -1
-          }
-          return return1 + return2 + return3
-        })
+        .setFilter((e) => doesCeramObjectPassesSearch(e?.properties, value))
         .on('ready', function (e) {
           e.target.eachLayer(function (layer) {
-            let archimageURL
-            let identifier
-
-            if (layer.feature.properties.Archimage == undefined) {
-              archimageURL = 'Archimage non disponible<br>'
-            } else {
-              archimageURL =
-                "<img src='https://archimage.efa.gr/action.php?kroute=image_preview_public&id=" +
-                layer.feature.properties.Archimage +
-                "&type=2&ext=.jpg' width='92%' margin-left='4%' /><br>"
-            }
-            if (layer.feature.properties.Inv_Fouille == null) {
-              identifier = ''
-            } else {
-              identifier = '<br>Type : ' + layer.feature.properties.Type
-            }
-            if (layer.feature.properties.Pi !== null) {
-              identifier = layer.feature.properties.Pi + 'Π'
-            }
-            layer.bindPopup(
-              archimageURL +
-                'Inventaire : ' +
-                identifier +
-                '<br>Identification : ' +
-                layer.feature.properties.Identification +
-                '<br>Type : ' +
-                layer.feature.properties.Type +
-                '<br>Description : ' +
-                layer.feature.properties.Description +
-                '<br>Bilbiographie : ' +
-                layer.feature.properties.Biblio +
-                "<br><a href='ceram?ID=" +
-                layer.feature.properties.ID +
-                '&THA' +
-                layer.feature.properties.Num_Analyse +
-                '&INV=' +
-                layer.feature.properties.Inv_Fouille +
-                "'>Fiche complète</a>",
-              {
-                maxWidth: 350,
-                minWidth: 350,
-                maxHeight: 550,
-                autoPan: true,
-                closeButton: true,
-                autoPanPadding: [5, 5]
-              }
-            )
+            setCeramLayer(layer)
             markerClusterGroupCeram.addLayer(layer)
           })
         })
@@ -127,44 +81,7 @@ export function searchCeramByClick(featureLayerCeram, markerClusterGroupCeram, m
     })
     .on('ready', function (e) {
       e.target.eachLayer(function (layer) {
-        let archimageURL
-
-        if (layer.feature.properties.ArchImage == undefined) {
-          archimageURL = 'Archimage non disponible<br>'
-        } else {
-          archimageURL =
-            "<img src='https://archimage.efa.gr/action.php?kroute=image_thumb&id=" +
-            layer.feature.properties.ArchImage +
-            "'/><br>"
-        }
-        layer.bindPopup(
-          archimageURL +
-            'Inventaire : ' +
-            // identifier + // TODO fix and check
-            '<br>Identification : ' +
-            layer.feature.properties.Identification +
-            '<br>Type : ' +
-            layer.feature.properties.Type +
-            '<br>Description : ' +
-            layer.feature.properties.Description +
-            '<br>Bilbiographie : ' +
-            layer.feature.properties.ETh7 +
-            "<br><a href='ceram?ID=" +
-            layer.feature.properties.ID +
-            '&ANA=THA' +
-            layer.feature.properties.Num_Analyse +
-            '&INV=' +
-            // identifier + // TODO fix and check
-            "'>Fiche complète</a>",
-          {
-            maxWidth: 350,
-            minWidth: 350,
-            maxHeight: 550,
-            autoPan: true,
-            closeButton: true,
-            autoPanPadding: [5, 5]
-          }
-        )
+        setCeramLayer(layer)
         markerClusterGroupCeram.addLayer(layer)
       })
     })
