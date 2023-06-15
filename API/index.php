493ecb82bd3b899b5211c94b973be3251467a170
AlZorba <jsgros3@gmail.com>
2023-06-15 20:09:30 +0200

    Merge API endpoints

--- API/index.php
+++ API/index.php
@@ -140,6 +140,45 @@ if (isset($_GET['GTh'])) {
         }
     }
 
+} elseif (isset($_GET['INV'])) {
+    // Return a json object with 3 lists of image urls. Example :
+// {
+//    "PHOTO":[
+//        "https:\/\/thacer.archaiodata.com\/API/IMAGES\/PHOTO\/758Π.JPG",
+//        "https:\/\/thacer.archaiodata.com\/API/IMAGES\/PHOTO\/758Π_2.JPG",
+//        "https:\/\/thacer.archaiodata.com\/API/IMAGES\/PHOTO\/758Π_3.JPG"
+//    ],
+//    "THA_MACRO_PHOTO":[
+//        "https:\/\/thacer.archaiodata.com\/API/IMAGES\/THA_MACRO_PHOTO\/THA23.JPG"
+//    ],
+//    "Profils":[
+//    ]
+//}
+
+    $result = ['PHOTO' => [], 'THA_MACRO_PHOTO' => [], 'Profils' => [],];
+    $INV = $_GET['INV'];
+    $ANA = $_GET['ANA'];
+
+    $dir = 'IMAGES/PHOTO/*' . $INV . '[Π_ -.]*{jpg,jpeg,JPG,gif,png}';
+    $files = glob($dir, GLOB_BRACE);
+    foreach ($files as $image) {
+        $result['PHOTO'][] = 'https://thacer.archaiodata.com/API/' . $image;
+    }
+
+    if ($ANA != '') {
+        $dir = 'IMAGES/THA_MACRO_PHOTO/*' . $ANA . '*.{jpg,jpeg,JPG,gif,png}';
+        $files = glob($dir, GLOB_BRACE);
+        foreach ($files as $image) {
+            $result['THA_MACRO_PHOTO'][] = 'https://thacer.archaiodata.com/API/' . $image;
+        }
+    }
+
+    $dir = 'IMAGES/Profils/*' . $INV . '[Π_ -.]*{jpg,jpeg,JPG,gif,png}';
+    $files = glob($dir, GLOB_BRACE);
+    foreach ($files as $image) {
+        $result['Profils'][] = 'https://thacer.archaiodata.com/API/' . $image;
+    }
+    $geojson = $result;
 } else {
     $error = '';
     $geojson = "400   Bad Request -  w or missing parameter";
